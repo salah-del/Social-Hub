11cd0b35eea371412ec2adf767ddead26e014856
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2025-01-07 10:44:39 +0200

    editing profile picture and cover in profile page

--- src/Components/socialHub/Profile/ProfileAndCover.jsx
+++ src/Components/socialHub/Profile/ProfileAndCover.jsx
@@ -4,6 +4,7 @@ import cover from "../../../assets/cover.png";
 import profile from "../../../assets/profile.jpg";
 import CoverPicture from "./Update/CoverPicture";
 import ProfilePicture from "./Update/ProfilePicture";
+import { isValidUrl } from "../../../Utils/validateURLs";
 const ProfileAndCover = ({ user, loading, edit }) => {
   return (
     <div>
@@ -13,7 +14,7 @@ const ProfileAndCover = ({ user, loading, edit }) => {
           <Skeleton height={155} />
         ) : (
           <img
-            src={user?.coverPicture || cover}
+            src={isValidUrl(user?.coverPicture) ? user?.coverPicture :  cover}
             alt="Something Wrong With Cover Picture"
             className="w-full h-[155px] object-cover"
           />
@@ -29,7 +30,7 @@ const ProfileAndCover = ({ user, loading, edit }) => {
           </div>
         ) : (
           <img
-            src={user?.profilePicture || profile}
+            src={isValidUrl(user?.profilePicture) ? user.profilePicture : profile}
             alt="profile"
             className="mr-5 Cover Picture -mt-11 w-20 h-20 sm:-mt-12 sm:w-[105px] sm:h-[105px] lg:w-36 lg:h-36 lg:-mt-20 rounded-full border-2 border-gray-300"
           />
